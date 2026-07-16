062089e0c205e30f0e5880cc86661c7c22618dce
indium114 <156162907+indium114@users.noreply.github.com>
2026-07-16 22:18:56 +0200

    fix: use proper reset code for colours

--- src/colour.rs
+++ src/colour.rs
@@ -3,54 +3,54 @@ use mlua::{Function, Lua};
 pub fn colours(state: &mut Lua) {
     // MARK: normal colours
     let black: Function = state
-        .create_function(|_, a: String| Ok("\x1b[0;30m".to_string() + &a + "\x1b[0;37m"))
+        .create_function(|_, a: String| Ok("\x1b[0;30m".to_string() + &a + "\x1b[0;0m"))
         .unwrap();
     let red: Function = state
-        .create_function(|_, a: String| Ok("\x1b[0;31m".to_string() + &a + "\x1b[0;37m"))
+        .create_function(|_, a: String| Ok("\x1b[0;31m".to_string() + &a + "\x1b[0;0m"))
         .unwrap();
     let green: Function = state
-        .create_function(|_, a: String| Ok("\x1b[0;32m".to_string() + &a + "\x1b[0;37m"))
+        .create_function(|_, a: String| Ok("\x1b[0;32m".to_string() + &a + "\x1b[0;0m"))
         .unwrap();
     let yellow: Function = state
-        .create_function(|_, a: String| Ok("\x1b[0;33m".to_string() + &a + "\x1b[0;37m"))
+        .create_function(|_, a: String| Ok("\x1b[0;33m".to_string() + &a + "\x1b[0;0m"))
         .unwrap();
     let blue: Function = state
-        .create_function(|_, a: String| Ok("\x1b[0;34m".to_string() + &a + "\x1b[0;37m"))
+        .create_function(|_, a: String| Ok("\x1b[0;34m".to_string() + &a + "\x1b[0;0m"))
         .unwrap();
     let purple: Function = state
-        .create_function(|_, a: String| Ok("\x1b[0;35m".to_string() + &a + "\x1b[0;37m"))
+        .create_function(|_, a: String| Ok("\x1b[0;35m".to_string() + &a + "\x1b[0;0m"))
         .unwrap();
     let cyan: Function = state
-        .create_function(|_, a: String| Ok("\x1b[0;36m".to_string() + &a + "\x1b[0;37m"))
+        .create_function(|_, a: String| Ok("\x1b[0;36m".to_string() + &a + "\x1b[0;0m"))
         .unwrap();
     let white: Function = state
-        .create_function(|_, a: String| Ok("\x1b[0;37m".to_string() + &a + "\x1b[0;37m"))
+        .create_function(|_, a: String| Ok("\x1b[0;37m".to_string() + &a + "\x1b[0;0m"))
         .unwrap();
 
     // MARK: bold colours
     let bold_black: Function = state
-        .create_function(|_, a: String| Ok("\x1b[1;30m".to_string() + &a + "\x1b[0;37m"))
+        .create_function(|_, a: String| Ok("\x1b[1;30m".to_string() + &a + "\x1b[0;0m"))
         .unwrap();
     let bold_red: Function = state
-        .create_function(|_, a: String| Ok("\x1b[1;31m".to_string() + &a + "\x1b[0;37m"))
+        .create_function(|_, a: String| Ok("\x1b[1;31m".to_string() + &a + "\x1b[0;0m"))
         .unwrap();
     let bold_green: Function = state
-        .create_function(|_, a: String| Ok("\x1b[1;32m".to_string() + &a + "\x1b[0;37m"))
+        .create_function(|_, a: String| Ok("\x1b[1;32m".to_string() + &a + "\x1b[0;0m"))
         .unwrap();
     let bold_yellow: Function = state
-        .create_function(|_, a: String| Ok("\x1b[1;33m".to_string() + &a + "\x1b[0;37m"))
+        .create_function(|_, a: String| Ok("\x1b[1;33m".to_string() + &a + "\x1b[0;0m"))
         .unwrap();
     let bold_blue: Function = state
-        .create_function(|_, a: String| Ok("\x1b[1;34m".to_string() + &a + "\x1b[0;37m"))
+        .create_function(|_, a: String| Ok("\x1b[1;34m".to_string() + &a + "\x1b[0;0m"))
         .unwrap();
     let bold_purple: Function = state
-        .create_function(|_, a: String| Ok("\x1b[1;35m".to_string() + &a + "\x1b[0;37m"))
+        .create_function(|_, a: String| Ok("\x1b[1;35m".to_string() + &a + "\x1b[0;0m"))
         .unwrap();
     let bold_cyan: Function = state
-        .create_function(|_, a: String| Ok("\x1b[1;36m".to_string() + &a + "\x1b[0;37m"))
+        .create_function(|_, a: String| Ok("\x1b[1;36m".to_string() + &a + "\x1b[0;0m"))
         .unwrap();
     let bold_white: Function = state
-        .create_function(|_, a: String| Ok("\x1b[1;37m".to_string() + &a + "\x1b[0;37m"))
+        .create_function(|_, a: String| Ok("\x1b[1;37m".to_string() + &a + "\x1b[0;0m"))
         .unwrap();
 
     let colour_table = state.create_table().unwrap();
