e0ad9c0798b6f5b5ddfb4238d5ecdfe708ab3174
indium114 <156162907+indium114@users.noreply.github.com>
2026-07-16 18:13:29 +0200

    feat: load the config file

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,38 @@
-use mlua::Lua;
+use std::{fs, process::exit};
+
+use mlua::{Function, Lua};
 
 mod stat;
 mod stat_wm;
 
+fn config_dir() -> String {
+    let home = dirs::home_dir()
+        .map(|p| p.to_string_lossy().into_owned())
+        .unwrap_or_default();
+    home + "/.config/haal"
+}
+
 fn main() {
     let mut lua = Lua::new();
+    let config = match fs::read_to_string(config_dir() + "/init.lua") {
+        Ok(cfg) => cfg,
+        Err(_) => {
+            println!("Failed to read config at {}", config_dir() + "/init.lua");
+            println!("(make sure the file exists and is readable)");
+            exit(1);
+        }
+    };
 
     stat::stat(&mut lua);
+    lua.load(config).exec().unwrap();
 
-    lua.load(
-        r#"
-            print("Window Manager: " .. wm.name)
-        "#,
-    )
-    .exec()
-    .unwrap();
+    let main_func: Function = match lua.globals().get("main") {
+        Ok(func) => func,
+        Err(_) => {
+            println!("No main function specified.");
+            exit(1)
+        }
+    };
+
+    main_func.call::<()>(()).unwrap();
 }
